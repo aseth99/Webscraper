d0c635ec910a06664b65f249251f1c329a2d8e1c
Aman Seth <aseth@ualberta.ca>
2018-11-14 09:49:32 +0100

    pull some crypto inf

---
+++ main.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://api.coinbase.com/v2/prices/USD/spot?'
+response = requests.get(url).json()
+
+for x in range(11):
+	print((response['data'][x]['currency'])+" "+(response['data'][x]['base'])+" "+(response['data'][x]['amount']))
+
+
+#response: {'data': [{'currency': 'USD', 'base': 'BTC', 'amount': '7590.01'}, {'currency':'USD', 'base': 'ETH', 'amount': '296.86'}, {'currency': 'USD', 'base': 'LTC', 'amount': '54.59'}]}
+
+# print(response)
+# soup = BeautifulSoup(response.content, 'html.parser')
+# seven_day = soup.find(id="seven-day-forecast")
+# bitcoin = soup.find('pre',{'style':'word-wrap: break-word; white-space: pre-wrap;'})
+
+# print(bitcoin)
