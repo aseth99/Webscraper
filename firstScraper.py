0906c4cc1bc16995ff2444c67e145698ba18b95a
Aman Seth <aseth@ualberta.ca>
2018-11-14 14:56:15 +0100

    learning from youtube, beautiful soup

--- firstScraper.py
+++ firstScraper.py
@@ -1,4 +1,7 @@
 from urllib.request import urlopen
+from bs4 import BeautifulSoup
 
 html = urlopen("http://pythonscraping.com/pages/page1.html")
-print(html.read())
+bsObj = BeautifulSoup(html.read(), 'lxml')
+
+print(bsObj.h1)
