33d2856ead6a53d9dc427aeb691086952f35707d
Aman Seth <aseth@ualberta.ca>
2018-11-15 11:18:17 +0100

    dk what changed lol

--- firstScraper.py
+++ firstScraper.py
@@ -6,6 +6,18 @@ html = urlopen("http://pythonscraping.com/pages/page1.html")
 bsObj = BeautifulSoup(html.read(), 'lxml')
 
 print(bsObj.h1)
-#print(bsObj.nonExtinetTAg.sometag) --> attribute error, object doesnt have attribute sometag
+#print(bsObj.nonExistentTag.sometag) --> attribute error, object doesnt have attribute sometag
 
 
+
+
+#psuedocode
+try:
+	badContent = bsObj.nonExistentTag.anotherTag
+except AttributeError as e:
+	print("Tag not found...")
+else: 
+	if badContent == None:
+		print("Tag not found...")
+	else:
+		print(badContent)
