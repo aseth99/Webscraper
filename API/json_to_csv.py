7ab46eb63a9ff1cfb3d711ed204fec9341389928
Aman Seth <aseth@ualberta.ca>
2018-11-22 14:51:04 +0100

    reformatting the csv, finding properties of tweets

--- API/json_to_csv.py
+++ API/json_to_csv.py
@@ -9,23 +9,39 @@ import pandas as pd
 # data_json = open('user_timeline_WeedFeed.json', mode='r').read() #reads in the JSON file into Python as a string
 # data_python = json.loads(data_json) #turns the string into a json Python object
 
-csv_out = open('tweets_out_ASCII.csv', mode='w') #opens csv file
+# user = sys.argv[1] #uses first arguement that we enter in command
+# client = get_twitter_client()
+# fname = "user_timeline_{}.json".format(user)
+
+# with open(fname, 'w') as f:
+# for page in Cursor(client.user_timeline, screen_name=user, count=200).pages(16):
+# for status in page:
+# f.write(json.dumps(status._json)+"\n")
+
+csvFileName = "tweets_Out.csv"
+csv_out = open(csvFileName, mode='w') #opens csv file
 writer = csv.writer(csv_out) #create the csv writer object
 
-fields = ['created_at', 'text', 'screen_name', 'followers', 'friends', 'rt', 'fav'] #field names
+fields = ['Twitter Handle & User Name', 'Tweet', ' external URL', 'Date of Tweet', 'Followers', 'Following', 'RT', 'FAV'] #field names
 writer.writerow(fields) #writes field
 
 tweets = []
-for line in open('user_timeline_WeedFeed.json', 'r'):
+for line in open('home_timeline_test.json', 'r'):
     tweets.append(json.loads(line))
 
 for line in tweets:
- 
-    #writes a row and gets the fields from the json object
+ 	
+    urlvar = line.get('entities').get('urls')
+    if(urlvar):
+    	urlvar = urlvar[0].get('expanded_url')
+    else:
+    	urlvar = "no external urls in this tweet"
+    #writes a row and gets the fields from the (now pyton) dict
     #screen_name and followers/friends are found on the second level hence two get methods
-    writer.writerow([line.get('created_at'),
+    writer.writerow([line.get('user').get('screen_name')+" , "+line.get('user').get('name'),
                      line.get('text').encode('unicode_escape'), #unicode escape to fix emoji issue
-                     line.get('user').get('screen_name'),
+	                 urlvar,
+                     line.get('created_at'),
                      line.get('user').get('followers_count'),
                      line.get('user').get('friends_count'),
                      line.get('retweet_count'),
@@ -34,7 +50,20 @@ for line in tweets:
 csv_out.close()
 
 
-
+#line.get('entities').get('urls').get("expanded_url"),
+# "entities": 
+# {
+# 	"hashtags": [], 
+# 	"symbols": [], 
+# 	"user_mentions": [], 
+# 	"urls": 
+# 		[{
+# 		"url": "https://t.co/v4eTGXIChd", 
+# 		"expanded_url": "https://goo.gl/6YkRg2", 
+# 		"display_url": "goo.gl/6YkRg2", 
+# 		"indices": [46, 69]
+# 		}]
+# }, 
 # f = open('home_timeline.json')
 # data = json.load(f)
 # f.close()
--- API/twitter_get_home_timeline.py
+++ API/twitter_get_home_timeline.py
@@ -1,11 +1,22 @@
 import json
+import sys
 from tweepy import Cursor
 from twitter_scraper_client import get_twitter_client
 
+# user = sys.argv[1] #uses first arguement that we enter in command
+# client = get_twitter_client()
+# fname = "user_timeline_{}.json".format(user)
+
+# with open(fname, 'w') as f:
+	# for page in Cursor(client.user_timeline, screen_name=user, count=200).pages(16):
+		# for status in page:
+			# f.write(json.dumps(status._json)+"\n")
+
 
 if __name__ == '__main__':
+	testName = sys.argv[1]
 	client = get_twitter_client()
-
+	fname = "home_timeline_{}.json".format(testName)
 	# #prints out the x number of tweets at top of timeline
 	# for status in Cursor(client.home_timeline).items(100):
 
@@ -16,7 +27,7 @@ if __name__ == '__main__':
 	#jsonl file allows file to be split, easier to process one line at a time
 
 	#limits to 800 from our own timeline, 3200 from a specific user 
-	with open('home_timeline.jsonl', 'w') as f:
+	with open(fname, 'w') as f:
 		for page in Cursor(client.home_timeline, count=200).pages(4):
 			for status in page:
 				f.write(json.dumps(status._json)+"\n")
